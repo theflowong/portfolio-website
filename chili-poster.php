8891a00edc2e3f93342efa52e632b37a8d769194
Florence Wong <florencewong1020@gmail.com>
2017-02-28 16:13:00 -0600

    add vegan-chili page and images

---
+++ chili-poster.php
@@ -0,0 +1,116 @@
+<?php include "header.php";?>
+
+		<div id = "container">
+
+			<div class="scroll-gallery_container xrow">
+
+				<div class="scroll-gallery_text">
+
+					<div class="title">
+						<div class="title-main">
+							Compassionate Action for Animals'<br>
+							8th Annual Vegan Chili Poster
+						</div>
+						<div class="title-info">
+							Canva, Photoshop<br>
+							2017
+						</div>
+					</div>
+
+					<div class="description">
+						<article>
+								<p>This February marked the 8th annual vegan chili cookoff
+								held by Minneapolis animal-rights organization
+								Compassionate Action for Animals.</p>
+                <p>I had a fun time creating a poster for the event,
+									optimized for print and Facebook.
+                </p>
+						</article>
+					</div>
+				</div>
+
+
+				<div class="scroll-gallery_art">
+					<ul>
+
+            <!--
+            <li>
+              <div class="art-description" id="tabah_all">
+                <div class="title">
+                  <div class="title-main">
+                    Tabah
+                  </div>
+                  <div class="title-info">
+                    Watercolor on paper, dimensions?<br>
+                    2017
+                  </div>
+                </div>
+                <p>
+                  Tabah album jacket covers!
+                </p>
+              </div>
+            </li>
+            -->
+
+            <li>
+              <a class="toggle-description" id="chili-poster0">
+                <img class="img_hover" src="img/vegan-chili/chili-poster-color.png" height="100%" border="0" />
+              </a>
+            </li>
+
+            <li>
+              <a class="toggle-description" id="chili-poster-fb0">
+                <img class="img_hover" src="img/vegan-chili/chili-poster-fb.png" height="100%" border="0" />
+              </a>
+            </li>
+
+
+
+              <!--
+							<li>
+								<a class="toggle-description" id="wild0">
+									<img class="img_hover" src="img/oil/surrealist1.jpg" height="100%" border="0" />
+								</a>
+							</li>
+							<li>
+								<div class="art-description" id="wild">
+									<div class="title">
+										<div class="title-main">
+											Wild
+										</div>
+										<div class="title-info">
+											Oil on canvas, 30"x20"<br>
+											2015
+										</div>
+									</div>
+									<p>
+										<em>"Wild"</em> is a hybrid oil painting of
+										Salvador Dali's "The Dream" and
+										Henri Rousseau's "Sleeping Gypsy."
+									</p>
+								</div>
+							</li>
+
+							<li>
+								<img class="img_hover" src="img/oil/surrealist2.jpg" height="100%" border="0" />
+							</li>
+
+							<li>
+								<img class="img_hover" src="img/oil/surrealist3.jpg" height="100%" border="0" />
+							</li>
+
+							<li>
+								<img class="img_hover" src="img/oil/surrealist4.jpg" height="100%" border="0" />
+							</li>
+
+							<li>
+								<img class="img_hover" src="img/oil/surrealist5.jpg" height="100%" border="0" />
+							</li>
+            -->
+
+					</ul>
+				</div>
+			</div>
+		</div>
+
+<?php include "footer.php"; ?>
