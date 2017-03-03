798faa0ac344e5570eb163115dfa9eba64b5a77d
Florence Wong <florencewong1020@gmail.com>
2017-03-03 15:23:53 -0600

    better-specify menu destinations

--- menu.php
+++ menu.php
@@ -1,6 +1,8 @@
 <ul id = "menu" class="nav-list">
-	<li class="nav-item"><a href="index.php">Art Gallery</a>
+	<li class="nav-item"><a href="index.php#artPortfolio">Portfolio</a>
 	</li>
-	<li class="nav-item"><a href="about.php">About Flo</a>
+	<li class="nav-item"><a href="about.php">About</a>
+	</li>
+	<li class="nav-item"><a href="contact.php">Work with Me</a>
 	</li>
 </ul>
