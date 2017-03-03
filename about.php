edfae58b8953a2dcd4889dc11f392ae1ec6890ad
Florence Wong <florencewong1020@gmail.com>
2017-03-03 15:24:09 -0600

    shuffle content for about/contact

--- about.php
+++ about.php
@@ -16,11 +16,10 @@
 						<p>Here are a few things I'm always actively trying to work on:</p>
 						<ul>
 							<li>strengthening communities</li>
-							<li>promoting equal opportunity</li>
-							<li>breaking stereotypes</li>
 							<li>having respect and compassion for all life forms</li>
 							<li>practicing nonviolent communication</li>
-							<li>encouraging moral reflection and consistency</li>
+							<li>encouraging moral dialogue, reflection and consistency</li>
+							<li>living intentionally</li>
 							<li>raising awareness and compassion for animals in the food industry</li>
 
 						</ul>
@@ -41,7 +40,8 @@
 							strong communities and people — I find myself
 							constantly inspired by those around me,
 							and feeling lucky to be where I am.
-							I'm currently working at the Walker Art Center and Highpoint Center for Printmaking,
+							I'm currently working at the <a href="http://www.walkerart.org">Walker Art Center</a>
+							and <a href="http://highpointprintmaking.org">Highpoint Center for Printmaking</a>,
 							promoting the arts scene among local communities in Minneapolis.</p>
 						<p>I grew up in Hong Kong with TCKs
 							in an international school,
@@ -56,35 +56,6 @@
 							but there's a chance
 							I'm probably already in the process of fixing them!</p>
 						<br>
-					<h2>Want to exchange ideas?</h2>
-						<p>Flo is happy to work with fellow artists, vegans,
-							activists, humans, organizations and small businesses,
-							especially those promoting:
-							<ul>
-								<li>social and environmental justice</li>
-								<li>LGBTQIA+ and racial equality</li>
-								<li>help for international students and immigrants</li>
-								<li>plant-based advocacy</li>
-								<li>mental health and well-being</li>
-								<li>artwork</li>
-								<li>vegan/food blogs</li>
-								<li>community-based projects</li>
-								<li>nice things, or spreading kindness in general</li>
-							</ul>
-							She wears many hats, and is
-							available to help with
-							creative requests (small or large!)
-							with design, web development or arts. This can include:
-							<ul>
-								<li>Creating illustrations for articles, posts, online content</li>
-								<li>Getting feedback on website or app interfaces</li>
-								<li>Creating digital painting thank-you postcards</li>
-							</ul>
-
-							Shoot her a <a href="mailto:hello@flowong.com">message</a>
-							with any ideas, comments, or questions!</p>
-					<p>
-					</p>
 				</div>
 
 			</div>
